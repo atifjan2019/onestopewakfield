c0fbb0f942c5d35f3802d51b9161bd1c1aada3ed
khyberwear <atifjan2019@gmail.com>
2026-04-01 08:01:04 +0100

    refactor: remove scroll indicator from landing page section

--- src/app/page.js
+++ src/app/page.js
@@ -126,13 +126,6 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 opacity-50 animate-bounce">
-          <span className="text-xs font-bold tracking-widest uppercase writing-vertical-rl text-white">Scroll</span>
-          <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </div>
       </section>
 
       {/* Services Grid */}
