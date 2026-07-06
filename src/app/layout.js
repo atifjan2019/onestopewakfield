9cf82f1d9195270badec19bb3cf6d8e3cc0dd4b4
Atif Jan <atifjan2019@gmail.com>
2026-07-06 22:29:14 +0500

    feat: add Google Tag Manager integration to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -54,7 +54,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} antialiased`} suppressHydrationWarning>
+      <head>
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MX6CGZCN');`}
+        </Script>
+      </head>
       <body className="min-h-screen flex flex-col font-sans" suppressHydrationWarning>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX6CGZCN" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
